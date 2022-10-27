0401b7adafa106e1ea120c5c60e36d3b73e00ff5
Edgar Trejo <54153553+edtrelo@users.noreply.github.com>
2022-10-26 19:30:30 -0500

    Update SustitucionGauss.py
    Se agregó la modalidad de pivoteo parcial. Además, se modificó de tal forma que los argumentos no sean alterados en su estado, creando copias de ellos las cuales serán las manipuladas por el algoritmo.

--- SistemasEcuacionesLineales/SustitucionGauss.py
+++ SistemasEcuacionesLineales/SustitucionGauss.py
@@ -1,57 +1,57 @@
-import numpy as np
-
-def resolverSistema(A, b=None):
+def resolverSistema(M, v=None):
   """Resulve el Sistema de Ecuaciones mediante el 
-  método de Gauss y la sustitución hacia atrás.
+  método de Gauss y la sustitución hacia atrás. Además, realiza un pivoteo parcial.
   
   Args:
-    A(list of lists):
-      Si A es una matriz nxn, entonces se le considera
+    M(list of lists):
+      Si M es una matriz nxn, entonces se le considera
       como la matriz que contiene solo a los coeficientes
       de las variables del sistema. 
       
-      Si A es una matriz nxn+1, entonces se le considera como la matriz aumentada del sistema.
+      Si M es una matriz nxn+1, entonces se le considera como la matriz aumentada del sistema.
       
-    b(list or None):
-      si b es None, es porque A es la matriz aumentada. En otro caso, b es el vector independiente.
+    v(list or None):
+      si v es None, es porque M es la matriz aumentada. En otro caso, v es el vector independiente.
       
   Returns:
-    x(np.ndarray):
+    X(np.ndarray):
       vector solución del sistema de ecuaciones.
       
   Raises:
     Genera una excepción si el sistema tiene infinitas
     soluciones o no tiene solución."""
 
-  A = np.array(A)
+  A = np.array(M, dtype=np.float64)  #nota: si dejo aqui sin argumento dtype, redondea todo a enteros.
   n, _ = A.shape
 
-  def crearMatrizAumentada(A, b):
+  def crearMatrizAumentada(M, v):
     """Crea una matriz aumentada con el vector no
     homogéneo."""
     nonlocal n
-    b = np.array(b)
+    b = np.array(v, dtype = np.float64)
     b = b.reshape(n,1)
-    return np.append(A, b, axis=1)
+    return np.append(M, b, axis=1)
 
   def obtenerPivote(A, i = 0):
-    """Obtiene el renglón p para el cual A[p,i] no es
-    cero buscando desde el i renglón."""
+    """Obtiene el renglón p para el cual A[p,i] no es cero y además
+    es el de mayor valor absoluto en la columna i. La búsqueda se hace
+    desde la fila i."""
     nonlocal n
-    for p in range(i, n-1):
-      if A[p, i] != 0:
-        return p
-  
-    raise Exception("No existe Solución Única.")
+    mayor = max(A[i:, i], key = abs)
+    p = list(A[i:, i]).index(mayor) + i
+    if mayor == 0:
+      # si el pivote resulta ser cero, entonces no hay solución.
+      raise Exception("El sistema no tiene solución.")
+    return p
 
   def intercambiarRenglones(A, i, j):
     """Intercambia los renglones i y j."""
     A[[i, j]] = A[[j, i]]
 
-  # si b es None, entonces asumimos que A ya es la matriz
+  # si v es None, entonces asumimos que A ya es la matriz
   # aumentada.
-  if b is not None:
-    A = crearMatrizAumentada(A, b)
+  if v is not None:
+    A = crearMatrizAumentada(A, v)
 
   for i in range(0, n-1):
     # Recorremos las primeras n-1 columnas, pues queremos
@@ -67,9 +67,7 @@ def resolverSistema(A, b=None):
       # columna actual.
       m = A[j,i]/A[i,i]  # m es el valor por el que hay
       # que multiplicar la fila pivote para hacer ceros
-      A[j] = A[j]-m*A[i]
-
-    print(A,"\n")
+      A[j] = A[j] - m*A[i]
 
   if A[n-1, n-1] == 0:
     # Si resulta que al haber terminado el pivoteado, el
@@ -77,15 +75,15 @@ def resolverSistema(A, b=None):
     # cero entonces no tenemos solución.
     raise Exception("No existe Solución Única.")
   # creamos el vector solución
-  x = np.zeros(n)
+  X = np.zeros(n)
   # comenzamos con la sustituación hacia atrás.
-  x[n-1] = A[n-1, n]/A[n-1, n-1]
+  X[n-1] = A[n-1, n]/A[n-1, n-1]
   # recorremos desde atrás el resto de variables que nos
   # hacen falta. 
   for i in range(n-2, -1, -1): #el segundo argumento es non-inclusive.
     suma = 0
     for j in range(i+1, n):
-      suma += A[i,j]*x[j]
-    x[i] =  (A[i, n] - suma)/A[i,i]
+      suma += A[i,j]*X[j]
+    X[i] =  (A[i, n] - suma)/A[i,i]
 
-  return x
+  return X
